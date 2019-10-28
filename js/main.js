89946192edbd42a2cd9682d2d193acd49f046bc0
Josh King <josh.king@nfl.com>
2019-10-27 20:14:16 -0500

    Battlemetrics + Twitch Updates | Small Refactoring

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 var i = 0;
-var name1 = 'Aqua';
-var name2 = 'FPS';
-var speed = 200;
+const name1 = 'Aqua';
+const name2 = 'FPS';
+const speed = 200;
 
-typeWriter();
+typeWriter(() => {
+  if (i < name2.length) {
+    document.getElementById("name2").innerHTML += name2.charAt(i);
+    i++;
+    setTimeout(typeWriter2, speed);
+  }
+});
 
-function typeWriter() {
+function typeWriter(cb) {
   if (i < name1.length) {
     document.getElementById("name1").innerHTML += name1.charAt(i);
     i++;
@@ -14,20 +20,12 @@ function typeWriter() {
   else {
     document.getElementById("name1").innerHTML += '<span class="text-primary" id="name2"></span>'
     i= 0;
-    typeWriter2();
+    cb();
   }
-}
-
-function typeWriter2() {
-    if (i < name2.length) {
-      document.getElementById("name2").innerHTML += name2.charAt(i);
-      i++;
-      setTimeout(typeWriter2, speed);
-    }
 }
 
 new Twitch.Embed("twitch-embed", {
     width: "100%",
     height: 675,
-    channel: "aquafpsgaming"
-  });
+    channel: "aquafps"
+});
